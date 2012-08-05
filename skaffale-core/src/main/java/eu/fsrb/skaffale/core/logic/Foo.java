18326cfee5769a099ffdd7bfbca63bbef4496384
Roberto Bentivoglio <roberto.bentivoglio@gmail.com>
2012-08-05 10:27:31 +0200

    Initial empty project structure

---
+++ skaffale-core/src/main/java/eu/fsrb/skaffale/core/logic/Foo.java
@@ -0,0 +1,11 @@
+package eu.fsrb.skaffale.core.logic;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: robbent
+ * Date: 8/5/12
+ * Time: 10:26 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Foo {
+}
